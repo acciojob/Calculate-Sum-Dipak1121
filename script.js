0bc88f94a8cff306817181c99ff6d9db35111d81
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-29 13:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let body = document.querySelector("body");
+let h1 = document.getElementById("heading");
+let n1 = document.getElementById("n1");
+let n2 = document.getElementById("n2");
+let btn = document.getElementById("sum_btn");
+btn.addEventListener("click", onClick);
+function onClick(){
+	let para = document.createElement("p");
+	para.innerText = eval(`${n1.value}+${n2.value}`);
+	body.appendChild(para);
+}
